d436d38d715067880d583d5e2839be558a5f7406
Xiangyu Wang <wxyucs@gmail.com>
2022-06-06 14:56:19 +0800

    rename account

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,13 +7,13 @@ sudo apt install -y openssh-server vim g++ gcc make htop zsh screen protobuf-com
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 # Download .vimrc_vundle and install
-wget -O $HOME/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
+wget -O $HOME/.vimrc https://raw.githubusercontent.com/wxyucs/vimrc/master/.vimrc_vundle
 vim +PluginInstall +qall
 rm $HOME/.vimrc
 
 # Download .vimrc
-wget -O $HOME/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc
-wget -O $HOME/.vimrc_vundle https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
+wget -O $HOME/.vimrc https://raw.githubusercontent.com/wxyucs/vimrc/master/.vimrc
+wget -O $HOME/.vimrc_vundle https://raw.githubusercontent.com/wxyucs/vimrc/master/.vimrc_vundle
 
 # CtrlP
 cd ~/.vim
